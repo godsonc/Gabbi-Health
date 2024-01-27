b9184be5b31edf3029926a0da715b667f714fa1d
Godson C. <nwankwogodsonc@gmail.com>
2024-01-27 20:06:22 +0100

    login details

--- cypress/e2e/gb.cy.js
+++ cypress/e2e/gb.cy.js
@@ -1,5 +1,7 @@
 it('gabbi health', () => {
     cy.visit('/')
     cy.get('[data-testid="test-emailAddress"]')
+       .type('dola@yopmail.com')
     cy.get('[data-testid="test-password"]')
+       .type('P@ssword01{enter}')
 })
