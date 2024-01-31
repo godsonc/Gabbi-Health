85a1412024664e5c6c195d29b0cc9b4ac68364a9
Godson C. <nwankwogodsonc@gmail.com>
2024-01-31 19:04:11 +0100

    book appointment

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,6 +4,9 @@ it('gabbi health', () => {
        .type('dola@yopmail.com')
     cy.get('[data-testid="test-password"]')
        .type('P@ssword01{enter}')
-    cy.contains('Appointments')
+    cy.get('[href="/appointments"]')
       .click()
+    cy.get('div.cursor-pointer > .flex')
+     .click()
+
 })
