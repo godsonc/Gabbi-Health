c55eb543d7de2eb10b476a8a706a0686ebe61d75
Godson C. <nwankwogodsonc@gmail.com>
2024-02-02 11:35:00 +0100

    click

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,6 +8,8 @@ it('gabbi health', () => {
       .click()
     cy.get('div.cursor-pointer > .flex')
      .click()
+     cy.get('.md\:grid-cols-2 > :nth-child(1) > .grid > :nth-child(1)')
+        .click()
     //cy.get('.md\:grid-cols-2 > :nth-child(1) > .grid > :nth-child(1)')
       // .click()
 
